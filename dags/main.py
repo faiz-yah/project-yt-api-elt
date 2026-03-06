94fa13d26add83a5c9a2b536b5e003c6bf3c247f
Faiz Yah <faizyah321@gmail.com>
2026-03-07 00:30:10 +0800

    Added functions for data quality checks using SODA

--- dags/main.py
+++ dags/main.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 
 from api.video_stats import get_playlist_id, get_video_ids, get_video_stats, save_to_json
 from datawarehouse.dwh import staging_table, core_table
+from dataquality.soda import yt_elt_data_quality
 
 # Define local timezone
 local_tz = pendulum.timezone("Asia/Kuala_Lumpur")
@@ -22,6 +23,11 @@ default_args = {
     "start_date": datetime(2025, 1, 1, tzinfo=local_tz),
     # 'end_date': datetime(2030, 12, 31, tzinfo=local_tz),
 }
+
+
+###################################
+### Collect Data Using YT API ####
+##################################
 
 with DAG(
     dag_id='produce_json',
@@ -42,6 +48,11 @@ with DAG(
     
     playlist_id >> video_ids >> video_stats >> save_to_json_task
 
+
+#########################################
+### Update Postgres DB with JSON data ####
+########################################
+
 with DAG(
     dag_id='update_db',
     default_args=default_args,
@@ -58,3 +69,25 @@ with DAG(
     
     update_staging >> update_core 
     
+#############################################
+### Data Quality Checks of the data in DB ####
+#############################################
+
+staging_schema = 'staging'
+core_schema = 'core'
+
+with DAG(
+    dag_id='update_db',
+    default_args=default_args,
+    description='DAG to carry out data quality checks on data in DB',
+    schedule='0 16 * * *', # crontab. guru
+    catchup=False
+) as dag:
+    
+    # Define tasks
+     dq_staging = yt_elt_data_quality(staging_schema)
+     dq_core = yt_elt_data_quality(core_schema)
+    
+    # Define dependencies
+
+    dq_staging >> dq_core 
